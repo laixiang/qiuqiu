5f09def0b9456a5af484214b290931efd20529f7
laixiang <139930316@qq.com>
2021-06-21 14:06:09 +0800

    Add files via upload

---
+++ comm.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    $(".search_btn").bind("click", function () {
+        if (VerifySearch()) {
+            var keyword = $(".input_txt").val();
+            keyword = keyword.replace(/(^\s*)|(\s*$)/g, "");
+
+            window.location.href = "/s/" + encodeURIComponent(keyword);
+        }
+
+        return false;
+    });
+
+    $(".input_txt").bind("keyup", function (e) { if (e.keyCode == 13) { $(".search_btn").trigger("click"); } });
+
+});
+function VerifySearch() {
+    var keyword = $(".input_txt").val();
+    keyword = keyword.replace(/(^\s*)|(\s*$)/g, "");
+
+    var ret = keyword.length == 0 ? false : true;
+    if (!ret) { alert("请输入搜索内容"); }
+
+    return ret;
+}
